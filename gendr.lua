a3b855793fe99d6022a71be9a20784e9219bfdb2
shreeswifty <shreeswifty@gmail.com>
2019-12-16 00:47:17 -0500

    Update gendr.lua
    first attempt at parameterization w/lua

--- gendr.lua
+++ gendr.lua
@@ -5,47 +5,255 @@ local cs = require 'controlspec'
 
 local gendr = {}
 
-function gendr.params()
-  -- first set
-  params:add{type = "control", id = "init_cp", name = "init_cp",
-    controlspec = cs.new(0, 1, "lin", 0, 12, ""), action = engine.init_cp}
-  params:add{type = "control", id = "amp_dist", name = "amp_dist",
-    controlspec = cs.new(0, 1, "lin", 0, 6, ""), action = engine.amp_dist}
-  params:add{type = "control", id = "dur_dist", name = "dur_dist",
-    controlspec = cs.new(0, 1, "lin", 0, 6, ""), action = engine.dur_dist}
-  params:add{type = "control", id = "mod_osc", name = "mod_osc",
-    controlspec = cs.new(0, 1, "lin", 0, 10, ""), action = engine.mod_osc}
-  params:add{type = "control", id = "mod_adc", name = "mod_adc",
-    controlspec = cs.new(0, 1, "lin", 0, 0, ""), action = engine.mod_adc}
-  params:add{type = "control", id = "hz_min", name = "hz_min",
-    controlspec = controlspec.new(100, 8000, "exp", 10, 220), action = engine.hz_min}
-  params:add{type = "control", id = "hz_max", name = "hz_max",
-    controlspec = controlspec.new(100, 8000, "exp", 10, 220), action = engine.hz_max}
-  params:add{type = "control", id = "gate", name = "gate",
-    controlspec = cs.new(0, 1, "lin", 0, 1, ""), action = engine.gate}
-  -- second set
-  params:add{type = "control", id = "svf_hz", name = "svf_hz",
-    controlspec = cs.new(0, 6, "lin", 0, 0, ""), action = engine.svf_hz}
-  params:add{type = "control", id = "svf_res", name = "svf_res",
-    controlspec = cs.new(0, 10000, "lin", 0, 8, ""), action = engine.svf_res}
-  params:add{type = "control", id = "svf_lp", name = "svf_lp",
-    controlspec = cs.new(0, 1, "lin", 0, 0, ""), action = engine.svf_lp}
-  params:add{type = "control", id = "svf_hp", name = "svf_hp",
-    controlspec = cs.new(0.01, 10, "lin", 0, 0.05, ""), action = engine.svf_hp}
-  params:add{type = "control", id = "svf_bp", name = "svf_bp",
-    controlspec = cs.new(0, 2, "lin", 0, 0.1, ""), action = engine.svf_bp}
-  params:add{type = "control", id = "svf_peak", name = "svf_peak",
-    controlspec = cs.new(0, 1, "lin", 0, 0.9, ""), action = engine.svf_peak}
-  params:add{type = "control", id = "svf_notch", name = "svf_notch",
-    controlspec = cs.new(0.01, 10, "lin", 0, 1, ""), action = engine.svf_notch}
-
-  -- amp
-  params:add{type = "control", id = "amp", name = "amp",
-    controlspec = cs.new(0, 1, "lin", 0, 1, ""), action = engine.amp}
-
-  params:add{type = "control", id = "pan", name = "pan",
-    controlspec = cs.new(0, 1, "lin", -1, 1, ""), action = engine.pan}
-
+  
+  function init()
+    -- add some params for arcify to tap into
+      params:add {
+        type = "control",
+        id = "init_cp",
+        name = "Control Points",
+        controlspec = controlspec.new(1, 20, "lin", 0, 12),
+        action = function(value)
+            engine.init_cp(value)
+            redraw()
+        end
+    }
+    
+              params:add {
+        type = "control",
+        id = "osc_hz",
+        name = "Osc Hz",
+        controlspec = controlspec.new(0.01, 100, "exp", 10, 100),
+        action = function(value)
+            engine.osc_hz(value)
+            redraw()
+        end
+    }
+    
+    params:add {
+        type = "control",
+        id = "hz_min",
+        name = "Min Frequency",
+        controlspec = controlspec.new(1, 8000, "lin", 10, 220),
+        action = function(value)
+            engine.hz_min(value)
+            redraw()
+        end
+    }
+    
+     params:add {
+        type = "control",
+        id = "hz_max",
+        name = "Max Frequency",
+        controlspec = controlspec.new(1, 8000, "lin", 10, 220),
+        action = function(value)
+            engine.hz_max(value)
+            redraw()
+        end
+    }
+         params:add {
+        type = "control",
+        id = "gate",
+        name = "Gate",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.gate(value)
+            redraw()
+        end
+    }
+    
+          params:add {
+        type = "control",
+        id = "amp",
+        name = "Amp",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.amp(value)
+            redraw()
+        end
+    }
+    
+          params:add {
+        type = "control",
+        id = "dur_dist",
+        name = "Dur Dist",
+        controlspec = controlspec.new(0, 6, "lin", 0, 1),
+        action = function(value)
+            engine.dur_dist(value)
+            redraw()
+        end
+    }
+    
+          params:add {
+        type = "control",
+        id = "dur_scale",
+        name = "Dur Scale",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.dur_scale(value)
+            redraw()
+        end
+    }
+    
+           params:add {
+        type = "control",
+        id = "amp_dist",
+        name = "Amp Dist",
+        controlspec = controlspec.new(0, 6, "lin", 0, 1),
+        action = function(value)
+            engine.amp_dist(value)
+            redraw()
+        end
+    }
+    
+          params:add {
+        type = "control",
+        id = "amp_scale",
+        name = "Amp Scale",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.amp_scale(value)
+            redraw()
+        end
+    }
+    
+            params:add {
+        type = "control",
+        id = "svf_hz",
+        name = "Filter Freq",
+        controlspec = controlspec.new(0, 8000, "lin", 10, 220),
+        action = function(value)
+            engine.svf_hz(value)
+            redraw()
+        end
+    }
+    
+             params:add {
+        type = "control",
+        id = "svf_res",
+        name = "Filter Res",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.svf_res(value)
+            redraw()
+        end
+    }
+    
+            params:add {
+        type = "control",
+        id = "mod_osc",
+        name = "Modulation Osc",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.mod_osc(value)
+            redraw()
+        end
+    }
+    
+              params:add {
+        type = "control",
+        id = "mod_adc",
+        name = "Modulation Input",
+        controlspec = controlspec.new(0, 0, "lin", 0, 1),
+        action = function(value)
+            engine.mod_adc(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "ad_param",
+        name = "ad_param",
+        controlspec = controlspec.new(0.0001, 1, "lin", 0, 1),
+        action = function(value)
+            engine.ad_param(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "dd_param",
+        name = "dd_param",
+        controlspec = controlspec.new(0.0001, 1, "lin", 0, 1),
+        action = function(value)
+            engine.dd_param(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "svf_lp",
+        name = "Low Pass",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.svf_lp(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "svf_hp",
+        name = "high Pass",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.svf_hp(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "svf_bp",
+        name = "Bandpass",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.svf_bp(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "svf_notch",
+        name = "Notch",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.svf_notch(value)
+            redraw()
+        end
+    }
+    
+                 params:add {
+        type = "control",
+        id = "svf_peak",
+        name = "Filter Peak",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.svf_peak(value)
+            redraw()
+        end
+    }
+    
+                  params:add {
+        type = "control",
+        id = "fadeTime",
+        name = "Fade Time",
+        controlspec = controlspec.new(0, 1, "lin", 0, 1),
+        action = function(value)
+            engine.fadeTime(value)
+            redraw()
+        end
+    }
+ 
 end
 
 return gendr
+
+
+
+
+
